625cd82a81a3d4a2822fb8de0c4119c945f9fda7
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2023-01-29 17:49:12 -0800

    update domain

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -1,2 +1,2 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000/';
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://localhost:4000/graphql';
+export const GRAPHQL_SERVER = 'https://note-app-server-zq0o.onrender.com';
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://note-app-server-zq0o.onrender.com/graphql';
